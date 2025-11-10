7895f4ba8322c2c97b74247bf54fbffbfb735a2d
baghdasarovn <n.baghdasarov@getqr.com>
2025-11-10 19:09:58 +0400

    GETQR-332: Set up A/B testing infrastructure

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -5,6 +5,8 @@ import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
 import { AnalyticScriptsComponent } from "core/integration/analytic/components/analytic-scripts";
 import RootProviders from "./providers";
+import { getUserCookieService } from "core/services/cookie/user-session.service";
+import { AnalyticInitializer } from "@/ui/analytics/analytics-initializer";
 
 export const metadata = constructMetadata();
 
@@ -13,9 +15,12 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const { user } = await getUserCookieService();
+
   return (
     <html lang="en" className={cn(inter.className)}>
       <AnalyticScriptsComponent />
+      <AnalyticInitializer user={user} sessionId={user?.id} />
       <body>
         <Theme>
           <RootProviders>{children}</RootProviders>
