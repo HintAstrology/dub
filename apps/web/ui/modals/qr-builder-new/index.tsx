75c06e05a58b968d115473fa663fac54a7ba8530
vmatkovskiy <v.matkovskyi@hint.app>
2025-11-10 06:13:12 +0200

    save

--- apps/web/ui/modals/qr-builder-new/index.tsx
+++ apps/web/ui/modals/qr-builder-new/index.tsx
@@ -1,4 +1,2 @@
 export { CreateQRButton } from "./create-qr-button";
-export { QRBuilderCreateModal } from "./qr-builder-create.modal";
 export { QRBuilderNewModal } from "./qr-builder-modal";
-export { QRBuilderUpdateModal } from "./qr-builder-update.modal";
--- apps/web/ui/modals/qr-builder-new/qr-builder-create.modal.tsx
+++
@@ -1,3 +0,0 @@
-export const QRBuilderCreateModal = () => {
-  return <div>QRBuilderCreateModal</div>;
-};
