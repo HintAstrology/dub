3489f5fe1daa9c51534defb38ed831c1f3317686
esafaryan-getqr <e.safaryan@getqr.com>
2025-11-24 13:38:59 +0400

    titles

--- apps/web/ui/landing/components/qr-tabs/components/qr-tabs-title.tsx
+++ apps/web/ui/landing/components/qr-tabs/components/qr-tabs-title.tsx
@@ -4,15 +4,7 @@ import { SectionTitle } from "../../section-title";
 export const QrTabsTitle: FC = () => {
   return (
     <div className="mb-6 flex flex-col items-center justify-center gap-2 md:mb-12 md:gap-3">
-      <SectionTitle
-        titleFirstPart="Create Your"
-        highlightedTitlePart="QR Code"
-        className="hidden md:block"
-      />
-      <SectionTitle
-        titleFirstPart="Create Your QR Code"
-        className="block md:hidden"
-      />
+      <SectionTitle titleFirstPart="Create Your QR Code" />
 
       <p className="text-muted-foreground hidden max-w-4xl text-center text-base sm:block md:text-lg">
         Generate branded QR codes and track engagement in real time{" "}
